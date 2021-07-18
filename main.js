661c450b3c1bf5c086ec555a0ddb7da1eb590f89
sunne090 <seonhui7622@gmail.com>
2021-07-19 02:31:27 +0900

    js 파일 작성 예정(햄버거만 구현)

---
+++ main.js
@@ -0,0 +1,7 @@
+
+$("#h-btn-d").click(function() { 
+    $(".hamburger-menu-wrap,.page_cover").addClass("open"); 
+}); 
+$("#cancel").click(function() { 
+    $(".hamburger-menu-wrap,.page_cover").removeClass("open"); 
+});
